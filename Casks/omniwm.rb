0b16735d7abc65237215dba270298fab9478c6ef
dindin12138 <79844759+dindin12138@users.noreply.github.com>
2026-03-25 18:15:11 +0800

    update(omniwm): v0.4.3.1

--- Casks/omniwm.rb
+++ Casks/omniwm.rb
@@ -1,6 +1,6 @@
 cask "omniwm" do
-  version "0.4.2.1"
-  sha256 "4f453535753d857742ff551e6db728c31b9bf7da8d648035cec800e6dfb85a26"
+  version "0.4.3.1"
+  sha256 "3ab1ca6f193c1be55375e4a516c2b91a93641f08555e1fd5f2ae15c187dfdadb"
 
   url "https://github.com/BarutSRB/OmniWM/releases/download/v#{version}/OmniWM-v#{version}.zip"
   name "OmniWM"
